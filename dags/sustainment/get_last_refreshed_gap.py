7696d4ebbf7845be02566c8802034014cee3849c
Carlos H <carlos.hernandez@toronto.ca>
2020-11-06 12:26:25 -0500

    Fix import

--- dags/sustainment/get_last_refreshed_gap.py
+++ dags/sustainment/get_last_refreshed_gap.py
@@ -1,6 +1,7 @@
 from airflow.operators.python_operator import PythonOperator
 from airflow.utils.dates import days_ago
 from airflow.models import Variable
+from datetime import datetime
 from airflow import DAG
 from pathlib import Path
 import ckanapi
