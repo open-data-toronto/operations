e1aaed9db5c847d2cb706526b208d581075a4572
Carlos H <carlos.hernandez@toronto.ca>
2020-11-10 16:31:23 -0500

    Syntax error fix

--- dags/datasets/covid_19_cases_in_toronto.py
+++ dags/datasets/covid_19_cases_in_toronto.py
@@ -1,4 +1,4 @@
-from airflow.operators.python_operator import PythonOperator, 
+from airflow.operators.python_operator import PythonOperator
 from airflow.operators.dummy_operator import DummyOperator
 from datetime import datetime
 from airflow.models import Variable
