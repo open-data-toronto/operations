040598d4d8e89f37bc7660b74accb2f84a33ee9a
Carlos H <carlos.hernandez@toronto.ca>
2020-11-09 19:58:25 -0500

    scheduling fix

--- dags/sustainment/update_data_quality_scores/update_data_quality_scores.py
+++ dags/sustainment/update_data_quality_scores/update_data_quality_scores.py
@@ -20,8 +20,8 @@ import dags.sustainment.update_data_quality_scores.dqs_logic as dqs_logic  # noq
 
 job_settings = {
     "description": "Calculates DQ scores across the catalogue",
-    "schedule": "0 16 * * 2,4",
-    "start_date": datetime(2020, 11, 9, 0, 30, 0),
+    "schedule": "@daily",
+    "start_date": datetime(2020, 11, 10, 0, 0, 0),
 }
 
 job_file = Path(os.path.abspath(__file__))
