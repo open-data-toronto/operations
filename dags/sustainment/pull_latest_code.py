c16aa9e4e9034d58e9486f0031e8557f7eaa724f
Carlos <carlos.hernandez@toronto.ca>
2021-02-10 12:51:59 -0500

    Experimenting with listing dags

--- dags/sustainment/pull_latest_code.py
+++ dags/sustainment/pull_latest_code.py
@@ -1,9 +1,10 @@
+from airflow.operators.python_operator import PythonOperator
 from airflow.operators.bash_operator import BashOperator
-from airflow.operators.dummy_operator import DummyOperator
-from datetime import datetime
 from airflow.models import Variable
 from airflow import DAG
+from datetime import datetime
 from pathlib import Path
+import logging
 import sys
 import os
 
@@ -17,6 +18,16 @@ job_settings = {
     "start_date": datetime(2020, 11, 10, 0, 30, 0),
     "schedule": "@once",
 }
+
+
+def compare_changed_to_list(**kwargs):
+    changes = kwargs.pop("ti").xcom_pull(task_ids="pull_repo")
+
+    dags = kwargs.pop("ti").xcom_pull(task_ids="list_dags")
+
+    logging.info(f"changes: {changes}")
+    logging.info(f"list_dags: {list_dags}")
+
 
 job_file = Path(os.path.abspath(__file__))
 job_name = job_file.name[:-3]
@@ -36,14 +47,23 @@ with DAG(
     catchup=False,
 ) as dag:
 
-    start = DummyOperator(task_id="start")
-
-    pull_code = BashOperator(
-        task_id="pull_code",
+    pull_repo = BashOperator(
+        task_id="pull_repo",
         bash_command=f"git -C {repo_dir} pull; echo $?",
         xcom_push=True,
     )
 
-    end = DummyOperator(task_id="end")
+    list_dags = BashOperator(
+        task_id="list_dags",
+        bash_command="airflow list_dags; echo $?",
+        xcom_push=True,
+    )
 
-    start >> pull_code >> end
+    compare = PythonOperator(
+        task_id="compare",
+        provide_context=True,
+        python_callable=compare_changed_to_list
+        )
+
+
+     pull_repo >> list_dags >> compare 
