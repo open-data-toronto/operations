0303def8e5fcc8329163ad118ba6f2d22558d167
Carlos <carlos.hernandez@toronto.ca>
2021-03-12 19:03:10 -0500

    changing to QA to wok afterhours since DEV is down

--- dags/datasets/traffic-volumes-at-intersections-for-all-modes.py
+++ dags/datasets/traffic-volumes-at-intersections-for-all-modes.py
@@ -30,7 +30,7 @@ JOB_NAME = JOB_FILE.name[:-3]
 PACKAGE_ID = JOB_NAME.replace("_", "-")
 
 # ACTIVE_ENV = Variable.get("active_env")
-ACTIVE_ENV = "dev"
+ACTIVE_ENV = "qa"
 CKAN_CREDS = Variable.get("ckan_credentials_secret", deserialize_json=True)
 CKAN = ckanapi.RemoteCKAN(**CKAN_CREDS[ACTIVE_ENV])
 SRC_FILES = "https://flashcrow-etladmin.intra.dev-toronto.ca/open_data/tmcs/"
