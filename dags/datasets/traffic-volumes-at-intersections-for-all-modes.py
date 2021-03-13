d8adb87f392dc27d5ccda42da13b894fb8d87599
Carlos <carlos.hernandez@toronto.ca>
2021-03-12 19:52:40 -0500

    Promoting job to PROD

--- dags/datasets/traffic-volumes-at-intersections-for-all-modes.py
+++ dags/datasets/traffic-volumes-at-intersections-for-all-modes.py
@@ -29,8 +29,7 @@ JOB_FILE = Path(os.path.abspath(__file__))
 JOB_NAME = JOB_FILE.name[:-3]
 PACKAGE_ID = JOB_NAME.replace("_", "-")
 
-# ACTIVE_ENV = Variable.get("active_env")
-ACTIVE_ENV = "qa"
+ACTIVE_ENV = Variable.get("active_env")
 CKAN_CREDS = Variable.get("ckan_credentials_secret", deserialize_json=True)
 CKAN = ckanapi.RemoteCKAN(**CKAN_CREDS[ACTIVE_ENV])
 SRC_FILES = "https://flashcrow-etladmin.intra.dev-toronto.ca/open_data/tmcs/"
