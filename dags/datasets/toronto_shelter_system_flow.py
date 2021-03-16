460774d65be3cc905105bbec21147453259bb401
Carlos <carlos.hernandez@toronto.ca>
2021-03-16 10:45:46 -0400

    Moving job to PROD

--- dags/datasets/toronto_shelter_system_flow.py
+++ dags/datasets/toronto_shelter_system_flow.py
@@ -29,8 +29,7 @@ JOB_FILE = Path(os.path.abspath(__file__))
 JOB_NAME = JOB_FILE.name[:-3]
 PACKAGE_ID = JOB_NAME.replace("_", "-")
 
-# ACTIVE_ENV = Variable.get("active_env")
-ACTIVE_ENV = "dev"
+ACTIVE_ENV = Variable.get("active_env")
 CKAN_CREDS = Variable.get("ckan_credentials_secret", deserialize_json=True)
 CKAN = ckanapi.RemoteCKAN(**CKAN_CREDS[ACTIVE_ENV])
 SRC_FILE = "http://opendata.toronto.ca/shelter.support.housing.administration/toronto-shelter-system-flow/toronto_shelter_system_flow.csv"  # noqa: E501
