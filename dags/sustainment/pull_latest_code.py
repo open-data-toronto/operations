3d9e9ec6cfa2682a7c1225e90427d77fe7a1a7d0
Carlos <carlos.hernandez@toronto.ca>
2021-04-12 10:30:16 -0400

    creating one-time ref file for vars

--- dags/sustainment/pull_latest_code.py
+++ dags/sustainment/pull_latest_code.py
@@ -5,6 +5,7 @@ from airflow import DAG
 from datetime import datetime
 from pathlib import Path
 import logging
+import json
 import sys
 import os
 
@@ -21,22 +22,43 @@ job_settings = {
 
 
 def compare_changed_to_list(**kwargs):
-    ti = kwargs.pop("ti")
-    changes = ti.xcom_pull(task_ids="pull_repo")
-    dags = ti.xcom_pull(task_ids="list_dags")
+    # ti = kwargs.pop("ti")
+    # changes = ti.xcom_pull(task_ids="pull_repo")
+    # dags = ti.xcom_pull(task_ids="list_dags")
 
-    logging.info(f"changes: {changes}")
-    logging.info(f"list_dags: {list_dags}")
+    # logging.info(f"changes: {changes}")
+    # logging.info(f"list_dags: {list_dags}")
+    keys = [
+        "active_env",
+        "backups_dir",
+        "ckan_credentials_secret",
+        "ckan_insert_chunk_size",
+        "flowworks_apikey",
+        "oracle_infinity_account_id",
+        "oracle_infinity_password",
+        "oracle_infinity_user",
+        "repo_dir",
+        "slack_dev_webhook_secret",
+        "slack_webhook_secret",
+        "tmp_dir",
+    ]
+
+    json_file = {}
+
+    for k in keys:
+        json_file[k] = Variable.get(k)
+
+    with open(Path(Variable.get(k)) + "/vars.json", "w") as f:
+        json.dump(json_file, f)
+
+    logging.info("created reference file")
 
 
 job_file = Path(os.path.abspath(__file__))
 job_name = job_file.name[:-3]
 
 default_args = airflow_utils.get_default_args(
-    {
-        "retries": 0,
-        "start_date": job_settings["start_date"],
-    }
+    {"retries": 0, "start_date": job_settings["start_date"],}
 )
 
 with DAG(
@@ -55,9 +77,7 @@ with DAG(
     )
 
     list_dags = BashOperator(
-        task_id="list_dags",
-        bash_command="airflow list_dags; echo $?",
-        xcom_push=True,
+        task_id="list_dags", bash_command="airflow list_dags; echo $?", xcom_push=True,
     )
 
     compare = PythonOperator(
