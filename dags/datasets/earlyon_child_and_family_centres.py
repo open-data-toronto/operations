16cb96586546ddf76662e0b4e2d049b21d2f9276
Carlos <carlos.hernandez@toronto.ca>
2021-04-30 12:38:16 -0400

    More logging

--- dags/datasets/earlyon_child_and_family_centres.py
+++ dags/datasets/earlyon_child_and_family_centres.py
@@ -67,7 +67,7 @@ with DAG(
         data = pd.DataFrame(response.json())
         filepath = Path(tmp_dir) / "new_data_raw.parquet"
         print(f"Filepath {filepath}")
-        print(f"Read {data.shape[0]} records")
+        print(f"Read {data.shape[0]} records. Data types {data.dtypes}")
         data.to_parquet(filepath)
 
         file_last_modified = response.headers["last-modified"]
