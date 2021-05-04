77a5c3799543b339f5f1796d3e655bc7b90474c2
Carlos <carlos.hernandez@toronto.ca>
2021-05-04 19:23:07 -0400

    Initial commit of package operator

---
+++ plugins/ckan_plugin/operators/package_operator.py
@@ -0,0 +1,17 @@
+import ckanapi
+from airflow.models.baseoperator import BaseOperator
+from airflow.utils.decorators import apply_defaults
+
+
+class GetPackageOperator(BaseOperator):
+    """
+    Returns package object from CKAN
+    """
+
+    @apply_defaults
+    def __init__(self, address, apikey, package_name_or_id):
+        self.package_id = package_name_or_id
+        self.ckan = ckanapi.RemoteCKAN(apikey=apikey, address=address)
+
+    def execute(self, context):
+        return self.ckan.action.package_show(id=self.package_id)
