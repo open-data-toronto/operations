63a9da22cfedce8a0da3782b8ef1a611f511ba96
Carlos <carlos.hernandez@toronto.ca>
2021-05-04 19:41:32 -0400

    kwargs fix

--- plugins/ckan_plugin/operators/package_operator.py
+++ plugins/ckan_plugin/operators/package_operator.py
@@ -9,9 +9,11 @@ class GetPackageOperator(BaseOperator):
     """
 
     @apply_defaults
-    def __init__(self, address, apikey, package_name_or_id):
+    def __init__(self, address, apikey, package_name_or_id, **kwargs):
+        super().__init__(**kwargs)
         self.package_id = package_name_or_id
         self.ckan = ckanapi.RemoteCKAN(apikey=apikey, address=address)
 
     def execute(self, context):
         return self.ckan.action.package_show(id=self.package_id)
+
