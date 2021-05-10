8a4510bd9f3046f7d8806a471bf0ec407ef6e457
Carlos <carlos.hernandez@toronto.ca>
2021-05-10 16:49:44 -0400

    experimentation again

--- plugins/ckan_plugin/ckan_plugin.py
+++ plugins/ckan_plugin/ckan_plugin.py
@@ -1,20 +1,18 @@
-from airflow.plugins_manager import AirflowPlugin
+# from airflow.plugins_manager import AirflowPlugin
 
-from operators.datastore_operator import *
-from operators.package_operator import *
-from operators.resource_operator import *
+# from operators import datastore_operator, package_operator, resource_operator
 
-# TODO: Releplace ckanapi library with standard HTTP requests (SimpleHTTPOperator)
+# # TODO: Releplace ckanapi library with standard HTTP requests (SimpleHTTPOperator)
 
 
-class CKANPlugin(AirflowPlugin):
-    name = "ckan_plugin"
-    operators = [
-        GetPackageOperator,
-        GetOrCreateResource,
-        ResourceAndFileOperator,
-        BackupDatastoreResourceOperator,
-        DeleteDatastoreResourceRecordsOperator,
-        InsertDatastoreResourceRecordsOperator,
-        RestoreDatastoreResourceBackupOperator,
-    ]
+# class CKANPlugin(AirflowPlugin):
+#     name = "ckan_plugin"
+#     operators = [
+#         package_operator.GetPackageOperator,
+#         resource_operator.GetOrCreateResource,
+#         resource_operator.ResourceAndFileOperator,
+#         datastore_operator.BackupDatastoreResourceOperator,
+#         datastore_operator.DeleteDatastoreResourceRecordsOperator,
+#         datastore_operator.InsertDatastoreResourceRecordsOperator,
+#         datastore_operator.RestoreDatastoreResourceBackupOperator,
+#     ]
