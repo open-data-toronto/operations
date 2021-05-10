71c5ed2beee27a7f38ef2a8649e0bb247223ee36
Carlos <carlos.hernandez@toronto.ca>
2021-05-10 17:10:48 -0400

    streamlining

--- dags/sustainment/pull_latest_code.py
+++ dags/sustainment/pull_latest_code.py
@@ -1,32 +1,17 @@
-from airflow.operators.bash import BashOperator
-
-from airflow.models import Variable
-from airflow import DAG
 from datetime import datetime
-from pathlib import Path
-import os
 
+from airflow import DAG
+from airflow.models import Variable
+from airflow.operators.bash import BashOperator
 from utils import airflow_utils
 
-job_settings = {
-    "description": "Pulls repo code. Updated dags must be deleted and restarted.",
-    "start_date": datetime(2020, 11, 10, 0, 30, 0),
-    "schedule": "@once",
-}
-
-
-job_file = Path(os.path.abspath(__file__))
-job_name = job_file.name[:-3]
-
-default_args = airflow_utils.get_default_args(
-    {"retries": 0, "start_date": job_settings["start_date"]}
-)
-
 with DAG(
-    job_name,
-    default_args=default_args,
-    description=job_settings["description"],
-    schedule_interval=job_settings["schedule"],
+    "pull_latest_code",
+    default_args=airflow_utils.get_default_args(
+        {"retries": 0, "start_date": datetime(2020, 11, 10, 0, 30, 0)}
+    ),
+    description="Pulls repo code. Updated dags must be deleted and restarted.",
+    schedule_interval="*/5 * * * *",
     tags=["sustainment"],
     catchup=False,
 ) as dag:
