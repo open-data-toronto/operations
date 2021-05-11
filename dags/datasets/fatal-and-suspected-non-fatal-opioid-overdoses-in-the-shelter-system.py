29b9353ef7d69b1939d4ee160200b766382f566e
Carlos <carlos.hernandez@toronto.ca>
2021-05-11 14:40:19 -0400

    initial commit of opiods job

--- dags/datasets/fatal-and-suspected-non-fatal-opioid-overdoses-in-the-shelter-system.py
+++ dags/datasets/fatal-and-suspected-non-fatal-opioid-overdoses-in-the-shelter-system.py
@@ -10,8 +10,6 @@ from airflow.models import Variable
 from airflow.operators.dummy import DummyOperator
 from airflow.operators.python import BranchPythonOperator, PythonOperator
 from airflow.utils.dates import days_ago
-
-# from airflow.utils.task_group import TaskGroup
 from ckan_operators.datastore_operator import (
     BackupDatastoreResourceOperator,
     DeleteDatastoreResourceRecordsOperator,
@@ -37,11 +35,12 @@ RESOURCES = {
         "file_url": "http://opendata.toronto.ca/shelter.support.housing.administration/fatal-and-non-fatal-suspected-opioid-overdoses-in-the-shelter-system/summary-suspected-opiod-overdoses-in-shelters.csv",  # noqa: E501
         "name": "summary-suspected-opiod-overdoses-in-shelters",
         "metadata": {
+            "package_id": PACKAGE_NAME,
             "format": "csv",
             "is_preview": False,
             "extract_job": f"Airflow: {PACKAGE_NAME}",
-            "url_type": "datastore",
         },
+        "datastore": True,
         "expected_columns": [
             "year",
             "year_stage",
@@ -53,11 +52,12 @@ RESOURCES = {
         "file_url": "http://opendata.toronto.ca/shelter.support.housing.administration/fatal-and-non-fatal-suspected-opioid-overdoses-in-the-shelter-system/suspected-opiod-overdoses-in-shelters.csv",  # noqa: E501
         "name": "suspected-opiod-overdoses-in-shelters",
         "metadata": {
+            "package_id": PACKAGE_NAME,
             "format": "csv",
             "is_preview": True,
             "extract_job": f"Airflow: {PACKAGE_NAME}",
-            "url_type": "datastore",
         },
+        "datastore": True,
         "expected_columns": [
             "site_name",
             "address",
@@ -96,15 +96,11 @@ with DAG(
 ) as dag:
 
     def is_resource_new(**kwargs):
-        pkg = kwargs["ti"].xcom_pull(task_ids="get_package")
+        package = kwargs["ti"].xcom_pull(task_ids="get_package")
         resource_name = kwargs["resource_name"]
 
-        logging.info(
-            f"looking for: {resource_name} | resources found: {[r['name'] for r in pkg['resources']]}"
-        )
-        is_new = resource_name not in [
-            r["name"] for r in pkg["resources"] if r["datastore_active"]
-        ]
+        logging.info(f"resources found: {[r['name'] for r in package['resources']]}")
+        is_new = resource_name not in [r["name"] for r in package["resources"]]
 
         prefix = "summary" if "summary" in resource_name else "granular"
 
@@ -119,15 +115,13 @@ with DAG(
 
         df = pd.read_csv(Path(data_file_info["path"]))
 
-        for col in [x.lower() for x in df.columns.values.tolist()]:
+        for col in df.columns.values:
             assert (
                 col in kwargs["expected_columns"]
             ), f"{col} not in list of expected columns"
 
         for col in kwargs["expected_columns"]:
-            assert col in [
-                x.lower() for x in df.columns.values.tolist()
-            ], f"Expected column {col} not in data file"
+            assert col in df.columns.values, f"Expected column {col} not in data file"
 
     def transform_data(**kwargs):
         ti = kwargs["ti"]
@@ -144,13 +138,11 @@ with DAG(
 
         return filepath
 
-    def build_data_dict(**kwargs):
-        ti = kwargs["ti"]
-        data_fp = Path(ti.xcom_pull(task_ids=kwargs["transform_data_task_id"]))
-        tmp_dir = Path(ti.xcom_pull(task_ids="tmp_dir"))
+        return "resource_is_not_new"
 
-        data = pd.read_parquet(data_fp)
-        fields_file_name = kwargs["fields_file_name"]
+    def build_data_dict(**kwargs):
+        data_fp = Path(kwargs["ti"].xcom_pull(task_ids="transform_data_task_id"))
+        data = pd.read_parquet(Path(data_fp))
         fields = []
 
         for col, col_type in data.dtypes.items():
@@ -165,20 +157,14 @@ with DAG(
 
             fields.append({"type": field_type, "id": col})
 
-        fields_path = tmp_dir / f"{fields_file_name}.json"
-        if not fields_path.exists():
-            with open(fields_path, "w") as f:
-                json.dump(fields, f)
+        return fields
 
-        return fields_path
-
-    def is_data_new(**kwargs):
+    def is_file_new(**kwargs):
         ti = kwargs["ti"]
-        backups_dir = Path(ti.xcom_pull(task_ids="backups_dir"))
-        backup_data = ti.xcom_pull(task_ids=kwargs["backup_data_task_id"])
         data_file_info = ti.xcom_pull(task_ids=kwargs["download_file_task_id"])
         resource = ti.xcom_pull(task_ids=kwargs["resource_task_id"])
-        prefix = "summary" if "summary" in kwargs["resource_name"] else "granular"
+
+        logging.info(f"resource: {resource} | data_file_info: {data_file_info}")
 
         last_modified_string = data_file_info["last_modified"]
         file_last_modified = parser.parse(last_modified_string)
@@ -192,10 +178,30 @@ with DAG(
         difference_in_seconds = (
             file_last_modified.timestamp() - resource_last_modified.timestamp()
         )
+
+        logging.info(
+            f"{difference_in_seconds}secs between file and resource last modified times"
+        )
+
+        prefix = "summary" if "summary" in kwargs["resource_name"] else "granular"
+
         if difference_in_seconds == 0:
             return f"{prefix}_file_is_not_new"
 
-        df = pd.read_parquet(Path(backup_data["data_file_path"]))
+        return f"{prefix}_file_is_new"
+
+    def is_data_new(**kwargs):
+        ti = kwargs["ti"]
+        fields = ti.xcom_pull(task_ids=kwargs["get_fields_task_id"])
+        if fields is not None:
+            return "data_is_new"
+
+        backups_dir = Path(ti.xcom_pull(task_ids="backups_dir"))
+        backup_data = ti.xcom_pull(task_ids=kwargs["backup_data_task_id"])
+
+        df = pd.read_parquet(backup_data["data"])
+        prefix = "summary" if "summary" in kwargs["resource_name"] else "granular"
+
         if df.shape[0] == 0:
             return f"{prefix}_data_is_new"
 
@@ -216,6 +222,19 @@ with DAG(
 
         return f"{prefix}_data_is_new"
 
+    def get_fields(**kwargs):
+        ti = kwargs["ti"]
+        backup_data = ti.xcom_pull(task_ids=kwargs["backup_data_task_id"])
+
+        if backup_data is not None:
+            with open(Path(backup_data["fields_file_path"]), "r") as f:
+                fields = json.load(f)
+        else:
+            fields = ti.xcom_pull(task_ids=kwargs["build_data_dict_task_id"])
+            assert fields is not None, "No fields"
+
+        return fields
+
     def build_notification_message(**kwargs):
         ti = kwargs.pop("ti")
         summary = kwargs.pop("summary_resource")
@@ -224,30 +243,28 @@ with DAG(
         lines = []
 
         # summary
-        summary_branch_path = ti.xcom_pull(task_ids="is_summary_data_new")
-        if summary_branch_path == "summary_file_is_not_new":
+        if "is_not_new" in ti.xcom_pull(task_ids="is_summary_file_new"):
             lines.append(f"- `{summary['name']}`: no new file")
         else:
             summary_sync_ts = ti.xcom_pull(task_ids="sync_summary_ts")
             last_modified = summary_sync_ts["file_last_modified"]
             line = f"- `{summary['name']}` last_modified synced to: {last_modified}"
 
-            if summary_branch_path == "summary_data_is_new":
+            if "is_new" in ti.xcom_pull(task_ids="summary_new_data_branch"):
                 summary_insert_count = ti.xcom_pull(task_ids="insert_summary_rows")
                 line = line + f" | records inserted: {summary_insert_count}"
 
             lines.append(line)
 
         # granular
-        granular_branch_path = ti.xcom_pull(task_ids="is_granular_data_new")
-        if granular_branch_path == "granular_file_is_not_new":
+        if "is_not_new" in ti.xcom_pull(task_ids="is_granular_file_new"):
             lines.append(f"- `{granular['name']}`: no new file")
         else:
             granular_sync_ts = ti.xcom_pull(task_ids="sync_granular_ts")
             last_modified = granular_sync_ts["file_last_modified"]
             line = f"- `{granular['name']}` last_modified synced to: {last_modified}"
 
-            if granular_branch_path == "granular_data_is_new":
+            if "is_new" in ti.xcom_pull(task_ids="granular_new_data_branch"):
                 granular_insert_count = ti.xcom_pull(task_ids="insert_granular_rows")
                 line = line + f" | records inserted: {granular_insert_count}"
 
@@ -344,38 +361,13 @@ with DAG(
     make_summary_data_dict = PythonOperator(
         task_id="make_summary_data_dict",
         python_callable=build_data_dict,
-        op_kwargs={
-            "transform_data_task_id": "transform_summary_data",
-            "fields_file_name": "summary_generated_fields",
-        },
+        op_kwargs={"transform_data_task_id": "transform_summary_data"},
     )
 
     make_granular_data_dict = PythonOperator(
         task_id="make_granular_data_dict",
         python_callable=build_data_dict,
-        op_kwargs={
-            "transform_data_task_id": "transform_granular_data",
-            "fields_file_name": "granular_generated_fields",
-        },
-    )
-
-    # create empty datastore resource with dictionary
-    insert_summary_data_dict = InsertDatastoreResourceRecordsOperator(
-        task_id="insert_summary_data_dict",
-        address=ckan_address,
-        apikey=ckan_apikey,
-        fields_json_path_task_id="make_summary_data_dict",
-        parquet_filepath_task_id=None,
-        resource_task_id="get_or_create_summary_resource",
-    )
-
-    insert_granular_data_dict = InsertDatastoreResourceRecordsOperator(
-        task_id="insert_granular_data_dict",
-        address=ckan_address,
-        apikey=ckan_apikey,
-        fields_json_path_task_id="make_granular_data_dict",
-        parquet_filepath_task_id=None,
-        resource_task_id="get_or_create_granular_resource",
+        op_kwargs={"transform_data_task_id": "transform_granular_data"},
     )
 
     # get package and get/create resources
@@ -411,7 +403,6 @@ with DAG(
         apikey=ckan_apikey,
         resource_task_id="get_or_create_summary_resource",
         dir_task_id="backups_dir",
-        trigger_rule="one_success",
     )
 
     backup_granular_data = BackupDatastoreResourceOperator(
@@ -420,7 +411,6 @@ with DAG(
         apikey=ckan_apikey,
         resource_task_id="get_or_create_granular_resource",
         dir_task_id="backups_dir",
-        trigger_rule="one_success",
     )
 
     # branch: if resource is not new, backup existing fields
@@ -440,34 +430,75 @@ with DAG(
     granular_resource_not_new = DummyOperator(task_id="granular_resource_is_not_new")
     granular_resource_is_new = DummyOperator(task_id="granular_resource_is_new")
 
-    # branch: data NOT new (data checksum), nothing to load
-    is_summary_data_new = BranchPythonOperator(
-        task_id="is_summary_data_new",
-        python_callable=is_data_new,
+    # get fields from backup (if resource is not new) or generated dict (if it is new)
+    get_summary_fields = PythonOperator(
+        task_id="get_summary_fields",
+        python_callable=get_fields,
+        trigger_rule="none_failed",
         op_kwargs={
+            "backup_data_task_id": "backup_summary_data",
+            "build_data_dict_task_id": "make_summary_data_dict",
+        },
+    )
+    get_granular_fields = PythonOperator(
+        task_id="get_granular_fields",
+        python_callable=get_fields,
+        trigger_rule="none_failed",
+        op_kwargs={
+            "backup_data_task_id": "backup_granular_data",
+            "build_data_dict_task_id": "make_granular_data_dict",
+        },
+    )
+
+    # branch: file NOT new (file_last_modified==resoure_last_modified), nothing to load
+    is_summary_file_new = BranchPythonOperator(
+        task_id="is_summary_file_new",
+        python_callable=is_file_new,
+        op_kwargs={
+            "download_file_task_id": "get_summary_data",
             "resource_name": summary_resource["name"],
             "resource_task_id": "get_or_create_summary_resource",
+        },
+    )
+    summary_file_not_new = DummyOperator(task_id="summary_file_is_not_new")
+    summary_file_is_new = DummyOperator(task_id="summary_file_is_new")
+
+    is_granular_file_new = BranchPythonOperator(
+        task_id="is_granular_file_new",
+        python_callable=is_file_new,
+        op_kwargs={
+            "download_file_task_id": "get_granular_data",
+            "resource_name": granular_resource["name"],
+            "resource_task_id": "get_or_create_granular_resource",
+        },
+    )
+    granular_file_not_new = DummyOperator(task_id="granular_file_is_not_new")
+    granular_file_is_new = DummyOperator(task_id="granular_file_is_new")
+
+    # branch: data NOT new (data checksum), nothing to load
+    summary_new_data_branch = BranchPythonOperator(
+        task_id="summary_new_data_branch",
+        python_callable=is_data_new,
+        op_kwargs={
+            "get_fields_task_id": "get_summary_fields",
+            "resource_name": summary_resource["name"],
             "backup_data_task_id": "backup_summary_data",
-            "download_file_task_id": "get_summary_data",
         },
     )
     summary_data_not_new = DummyOperator(task_id="summary_data_is_not_new")
     summary_data_is_new = DummyOperator(task_id="summary_data_is_new")
-    summary_file_is_not_new = DummyOperator(task_id="summary_file_is_not_new")
 
-    is_granular_data_new = BranchPythonOperator(
-        task_id="is_granular_data_new",
+    granular_new_data_branch = BranchPythonOperator(
+        task_id="granular_new_data_branch",
         python_callable=is_data_new,
         op_kwargs={
+            "get_fields_task_id": "get_granular_fields",
             "resource_name": granular_resource["name"],
-            "resource_task_id": "get_or_create_granular_resource",
             "backup_data_task_id": "backup_granular_data",
-            "download_file_task_id": "get_granular_data",
         },
     )
     granular_data_not_new = DummyOperator(task_id="granular_data_is_not_new")
     granular_data_is_new = DummyOperator(task_id="granular_data_is_new")
-    granular_file_is_not_new = DummyOperator(task_id="granular_file_is_not_new")
 
     # delete & insert records
     delete_summary_rows = DeleteDatastoreResourceRecordsOperator(
@@ -477,8 +508,8 @@ with DAG(
         backup_task_id="backup_summary_data",
     )
 
-    delete_granular_rows = DeleteDatastoreResourceRecordsOperator(
-        task_id="delete_granular_rows",
+    delete_summary_rows = DeleteDatastoreResourceRecordsOperator(
+        task_id="delete_summary_rows",
         address=ckan_address,
         apikey=ckan_apikey,
         backup_task_id="backup_granular_data",
@@ -523,7 +554,6 @@ with DAG(
         trigger_rule="one_success",
     )
 
-    # notification message
     build_message = PythonOperator(
         task_id="build_message",
         trigger_rule="none_failed",
@@ -538,57 +568,55 @@ with DAG(
         task_id="send_message", python_callable=send_notification_message,
     )
 
-    # cleanup
     delete_tmp_data = DeleteLocalDirectoryOperator(
         task_id="delete_tmp_data", path=Path(Variable.get("tmp_dir")) / PACKAGE_NAME,
     )
 
-    # sequence
-    tmp_dir >> get_summary_data >> validate_summary_expected_columns
-    validate_summary_expected_columns >> transform_summary_data
+    tmp_dir >> get_summary_data >> [
+        validate_summary_expected_columns,
+        is_summary_file_new,
+    ] >> transform_summary_data >> make_summary_data_dict
 
-    tmp_dir >> get_granular_data >> validate_granular_expected_columns
-    validate_granular_expected_columns >> transform_granular_data
+    tmp_dir >> get_granular_data >> [
+        validate_granular_expected_columns,
+        is_granular_file_new,
+    ] >> transform_granular_data >> make_granular_data_dict
 
-    backups_dir >> get_package
+    backups_dir >> [backup_summary_data, backup_granular_data]
 
-    get_package >> get_or_create_summary_resource >> is_summary_resource_new
-    transform_summary_data >> is_summary_resource_new
-
-    get_package >> get_or_create_granular_resource >> is_granular_resource_new
-    transform_granular_data >> is_granular_resource_new
+    get_package >> get_or_create_summary_resource >> [
+        is_summary_resource_new,
+        is_summary_file_new,
+    ]
+    get_package >> get_or_create_granular_resource >> [
+        is_granular_resource_new,
+        is_granular_file_new,
+    ]
 
     is_summary_resource_new >> [summary_resource_not_new, summary_resource_is_new]
-    summary_resource_not_new >> backup_summary_data
-    summary_resource_is_new >> make_summary_data_dict >> insert_summary_data_dict
-    insert_summary_data_dict >> backup_summary_data
-    backup_summary_data >> is_summary_data_new
+    summary_resource_not_new >> backup_summary_data >> get_summary_fields
+    summary_resource_is_new >> make_summary_data_dict >> get_summary_fields
 
     is_granular_resource_new >> [granular_resource_not_new, granular_resource_is_new]
-    granular_resource_not_new >> backup_granular_data
-    granular_resource_is_new >> make_granular_data_dict >> insert_granular_data_dict
-    insert_granular_data_dict >> backup_granular_data
+    granular_resource_not_new >> backup_granular_data >> get_granular_fields
+    granular_resource_is_new >> make_granular_data_dict >> get_granular_fields
 
-    granular_file_is_not_new >> build_message
-    backup_granular_data >> is_granular_data_new
+    is_summary_file_new >> [summary_file_not_new, summary_file_is_new]
+    summary_file_not_new >> build_message
+    summary_file_is_new >> summary_new_data_branch
 
-    is_summary_data_new >> [
-        summary_data_not_new,
-        summary_data_is_new,
-        summary_file_is_not_new,
-    ]
-    summary_file_is_not_new >> build_message
+    is_granular_file_new >> [granular_file_not_new, granular_file_is_new]
+    granular_file_not_new >> build_message
+    granular_file_is_new >> granular_new_data_branch
+
+    summary_new_data_branch >> [summary_data_not_new, summary_data_is_new]
     summary_data_not_new >> sync_summary_ts
     summary_data_is_new >> delete_summary_rows >> insert_summary_rows >> sync_summary_ts
-    sync_summary_ts >> build_message
 
-    is_granular_data_new >> [
-        granular_data_not_new,
-        granular_data_is_new,
-        granular_file_is_not_new,
-    ]
+    granular_new_data_branch >> [granular_data_not_new, granular_data_is_new]
     granular_data_not_new >> sync_granular_ts
-    granular_data_is_new >> delete_granular_rows >> insert_granular_rows
-    insert_granular_rows >> sync_granular_ts >> build_message
+    granular_data_is_new >> delete_summary_rows >> insert_granular_rows >> [
+        sync_granular_ts
+    ]
 
-    build_message >> send_message >> delete_tmp_data
+    sync_granular_ts >> build_message >> send_message >> delete_tmp_data
