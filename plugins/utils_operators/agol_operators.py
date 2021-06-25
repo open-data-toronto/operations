23046517649f496f5b73532e7e33faae017629d5
mackeynichols <mackey.nichols@gmail.com>
2021-06-25 18:40:12 +0000

    adding file for test cases, refactored file and agol operators and their tests

--- plugins/utils_operators/agol_operators.py
+++ plugins/utils_operators/agol_operators.py
@@ -17,12 +17,13 @@ class AGOLDownloadFileOperator(BaseOperator):
     """
     Downloads file from AGOL URL and saves to provided directory using provided filename.
     This will always overwrite an existing file, if it's there.
-    This is because we are not interested in versioning this data - only pulling the latest from AGOL.
+    This is because we are not interested in versioning AGOL data - only pulling the latest from AGOL.
 
     Returns a dictionary containing:
         - path: path to saved file containing data
         - last_modified: timestamp file was last_modified (from the request)
         - fields_path: path to file containing fields
+        - checksum: md5 hash of the download's contents
     """
 
     @apply_defaults
@@ -41,8 +42,10 @@ class AGOLDownloadFileOperator(BaseOperator):
         # init the filepath to the file we will create
         self.path = Path(self.dir) / self.filename
 
+
     def parse_data_from_agol(self):
-        res = agol_utils.get_data( self.file_url )
+        # calls agol utils to get only the features from a simple GET request to AGOL
+        res = agol_utils.get_features( self.file_url )
         last_modified = requests.get(self.file_url).headers["last-modified"]
         fields = agol_utils.get_fields( self.file_url )
         
@@ -51,13 +54,16 @@ class AGOLDownloadFileOperator(BaseOperator):
                  "fields": fields
                 }
 
+
     def write_to_file(self, data, filepath):
         # write the data to a file
         with open(filepath, "w") as f:
             f.write(data)
 
+        checksum = hashlib.md5()
+        checksum.update(data.encode('utf-8'))
 
-
+        return checksum
 
 
     def execute(self, context):
@@ -67,7 +73,7 @@ class AGOLDownloadFileOperator(BaseOperator):
 
         # write data to a file, get checksum
         data = json.dumps(res["data"])
-        self.write_to_file(data, self.path)
+        checksum = self.write_to_file(data, self.path)
 
         # write fields to a file
         fields = json.dumps(res["fields"])
@@ -75,16 +81,10 @@ class AGOLDownloadFileOperator(BaseOperator):
         fields_path = Path(self.dir) / fields_filename
         self.write_to_file(fields, fields_path)
 
-
-        #  Return dict with data's filepath, last modified date, and checksum
+        # Return dict with data's filepath, last modified date, and checksum
         return {
-            "path": self.path,
+            "data_path": self.path,
             "fields_path": fields_path,
             "last_modified": last_modified,
-            #"checksum": checksum.hexdigest(),
+            "checksum": checksum.hexdigest(),
         }
-
-
-
-    
-
--- tests/plugins/utils_operators/test_file_operators.py
+++ tests/plugins/utils_operators/test_file_operators.py
@@ -3,15 +3,12 @@
 import pytest
 import requests
 import os
-import hashlib
 
 from datetime import datetime
 
 from airflow import DAG
 from airflow.models import TaskInstance
 from utils_operators.file_operators import DownloadFileOperator
-
-#from airflow.operators import HTTPGETToFileOperator
 
 # init the url where the data will come from
 file_url = "https://contrib.wp.intra.prod-toronto.ca/app_content/tpp_measures/"
@@ -20,6 +17,11 @@ file_url_content = requests.get(file_url).text
 # init the directory where the data will be written to
 current_folder = os.path.dirname(os.path.realpath(__file__))
 dir = current_folder + "/tmp_dir"
+filename = "file_data.json"
+
+# make sure there isn't already a file where this test will be writing to
+if os.path.exists(dir + "/" + filename):
+    os.remove( dir + "/" + filename )
 
 # init the dag for the tests
 dag = DAG(dag_id='anydag', start_date=datetime.now())
@@ -29,7 +31,7 @@ task = DownloadFileOperator(
         task_id="get_data",
         file_url=file_url,
         dir=dir,
-        filename="file_data.json",
+        filename=filename,
         dag=dag
     )
 
@@ -47,7 +49,11 @@ def test_execute():
     assert "last_modified" in keys
     assert "checksum" in keys
 
-    # checks file is created in proper location and contains correct data
-    checksum = hashlib.md5()
-    checksum.update(file_url_content.encode('utf-8'))
-    assert checksum.hexdigest() == output["checksum"]
+    # checks file is created in proper location 
+    assert os.path.exists(dir + "/" + filename)
+
+    # checks if checksum is correct
+    with open( "/data/operations/tests/plugins/utils_operators/test_file_operators_checksum.txt" ) as f:
+        file_checksum = f.read()
+
+    assert file_checksum == output["checksum"]
