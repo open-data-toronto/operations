dfe3e8349a3ab6988eec9124098859c3925de969
mackeynichols <mackey.nichols@gmail.com>
2021-06-25 18:40:12 +0000

    adding file for test cases, refactored file and agol operators and their tests

--- tests/plugins/utils_operators/test_agol_operators.py
+++ tests/plugins/utils_operators/test_agol_operators.py
@@ -13,18 +13,21 @@ from airflow.models import TaskInstance
 from utils_operators.agol_operators import AGOLDownloadFileOperator
 from utils import agol_utils
 
-
-#from airflow.operators import HTTPGETToFileOperator
-
-# init the url where the data will come from
-file_url = "https://services.arcgis.com/S9th0jAJ7bqgIRjw/arcgis/rest/services/Reported_Crimes_ASR_RC_TBL_001/FeatureServer/0/query?where=1%3D1&outFields=*&outSR=4326&f=json"
-file_url_content = agol_utils.get_data(file_url)
-file_url_fields = agol_utils.get_fields(file_url)
+# init the base url, without query parameters, of where the data will come from
+file_url = "https://services3.arcgis.com/b9WvedVPoizGfvfD/arcgis/rest/services/COTGEO_CENSUS_NEIGHBORHOOD/FeatureServer/0/"
 
 
 # init the directory where the data will be written to
 current_folder = os.path.dirname(os.path.realpath(__file__))
 dir = current_folder + "/tmp_dir"
+filename = "agol_data.json"
+
+# make sure there isn't already a file where this test will be writing to
+if os.path.exists(dir + "/" + filename):
+    os.remove( dir + "/" + filename )
+
+if os.path.exists(dir + "/fields_" + filename):
+    os.remove( dir + "/fields_" + filename )
 
 # init the dag for the tests
 dag = DAG(dag_id='anydag', start_date=datetime.now())
@@ -34,7 +37,7 @@ task = AGOLDownloadFileOperator(
         task_id="get_data",
         file_url=file_url,
         dir=dir,
-        filename="agol_data.json",
+        filename=filename,
         dag=dag
     )
 
@@ -52,19 +55,29 @@ def test_execute():
     # checks all keys are present in operator output
     output = task.execute(ti.get_template_context())
     keys = output.keys()
-    assert "path" in keys
+    assert "data_path" in keys
+    assert "fields_path" in keys
     assert "last_modified" in keys
-    # assert "checksum" in keys
+    assert "checksum" in keys
 
 
     # checks if data file is created in proper location and contains correct data
-    with open( output["path"] ) as f:
+    with open( output["data_path"] ) as f:
         filedata = json.load(f)
-
-    assert filedata == file_url_content
+        assert filedata
 
     # checks if fields file is in proper location and contains correct fields
     with open( output["fields_path"] ) as f:
         fields_data = json.load(f)
 
-    assert fields_data == file_url_fields
+    with open( "/data/operations/tests/plugins/utils_operators/test_agol_operators_fields.json" ) as f:
+        test_fields_data = json.load(f)
+
+    assert fields_data == test_fields_data
+    
+
+    # checks if checksums match
+    with open( "/data/operations/tests/plugins/utils_operators/test_agol_operators_checksum.txt" ) as f:
+        file_checksum = f.read()
+    
+    assert output["checksum"] == file_checksum
