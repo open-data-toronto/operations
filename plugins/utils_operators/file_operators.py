732db1c1d6f6202a0415498bf9a8472cb5e32f1a
EC2 Default User <ec2-user@ip-10-162-44-148.ca-central-1.compute.internal>
2021-07-14 16:14:47 +0000

    adding ability for task file operator to read both task ids and keys or actual values as inputs

--- plugins/utils_operators/file_operators.py
+++ plugins/utils_operators/file_operators.py
@@ -12,6 +12,7 @@ from airflow.utils.decorators import apply_defaults
 class DownloadFileOperator(BaseOperator):
     """
     Downloads file from URL and saves to provided directory using provided filename.
+    Takes strings or reference to task ids + keys returning strings as inputs
 
     Returns a dictionary containing:
         - path: path to saved file
@@ -22,19 +23,35 @@ class DownloadFileOperator(BaseOperator):
     @apply_defaults
     def __init__(
         self,
-        file_url: str,
-        dir: str,
-        filename: str,
+        file_url: str = None,
+        dir: str = None,
+        filename: str = None,
         overwrite_if_exists: bool = True,
+        file_url_task_id: str = None,
+        file_url_task_key: str= None,
+        dir_task_id: str = None,
+        dir_task_key: str= None,
+        filename_task_id: str = None, 
+        filename_task_key: str= None,
         **kwargs,
     ) -> None:
         super().__init__(**kwargs)
-        self.file_url = file_url
-        self.dir = dir
-        self.filename = filename
+        # Assigning these vars in single lines to emphasize that they are alternatives to one another
+        self.file_url, self.file_url_task_id, self.file_url_task_key = file_url, file_url_task_id, file_url_task_key
+        self.dir, self.dir_task_id, self.dir_task_key = dir, dir_task_id, dir_task_key
+        self.filename, self.filename_task_id, self.filename_task_key = filename, filename_task_id, filename_task_key
+                    
         self.overwrite_if_exists = overwrite_if_exists
 
+    def set_path(self, ti):
         # init the filepath to the file we will create
+        # how this is done depends on whether the operator received task ids/keys, or actual values 
+        if self.dir_task_id and self.dir_task_key:
+            self.dir = ti.xcom_pull(task_ids=self.dir_task_id, key=self.dir_task_key)
+
+        if self.filename_task_id and self.filename_task_key:
+            self.filename = ti.xcom_pull(task_ids=self.filename_task_id, key=self.filename_task_key)
+
         self.path = Path(self.dir) / self.filename
 
     def overwrite_file_check(self):
@@ -57,7 +74,11 @@ class DownloadFileOperator(BaseOperator):
             "checksum": checksum.hexdigest(),
         }
 
-    def get_data_from_http_request(self):
+    def get_data_from_http_request(self, ti):
+        # get url if its from a separate task
+        if self.file_url_task_id and self.file_url_task_key:
+            self.file_url = ti.xcom_pull(task_ids=self.file_url_task_id, key=self.file_url_task_key)
+
         # grab data from input url
         res = requests.get(self.file_url)
         assert res.status_code == 200, f"Response status: {res.status_code}"
@@ -93,6 +114,12 @@ class DownloadFileOperator(BaseOperator):
 
 
     def execute(self, context):
+        # set task instance from context
+        ti = context['ti']
+
+        # create filepath for file well create
+        self.set_path(ti)
+
         # if the file exists already and we don't want to overwrite it
         if self.overwrite_file_check():
             result = self.return_current_file_metadata()
@@ -100,7 +127,7 @@ class DownloadFileOperator(BaseOperator):
         # if the file doesn't exist or we're ok with overwriting an existing one
         else:
             # get data from http request
-            res = self.get_data_from_http_request()
+            res = self.get_data_from_http_request(ti)
 
             # write response to file and get its md5 hash
             checksum = self.write_response_to_file_and_return_hash(res)
