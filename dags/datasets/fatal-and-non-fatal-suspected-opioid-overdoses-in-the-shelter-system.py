f6c38d03d9ec2031aa8ba9e518d402d6a503cbe0
mackeynichols <mackey.nichols@gmail.com>
2021-11-29 17:08:14 +0000

    fixing 'opiod' typo in opioid dag

--- dags/datasets/fatal-and-non-fatal-suspected-opioid-overdoses-in-the-shelter-system.py
+++ dags/datasets/fatal-and-non-fatal-suspected-opioid-overdoses-in-the-shelter-system.py
@@ -35,8 +35,8 @@ from utils_operators.file_operators import DownloadFileOperator
 PACKAGE_NAME = "fatal-and-non-fatal-suspected-opioid-overdoses-in-the-shelter-system"
 
 RESOURCES = {
-    "summary-suspected-opiod-overdoses-in-shelters": {
-        "file_url": "http://opendata.toronto.ca/shelter.support.housing.administration/fatal-and-non-fatal-suspected-opioid-overdoses-in-the-shelter-system/summary-suspected-opiod-overdoses-in-shelters_testQA.csv",  # noqa: E501
+    "summary-suspected-opioid-overdoses-in-shelters": {
+        "file_url": "http://opendata.toronto.ca/shelter.support.housing.administration/fatal-and-non-fatal-suspected-opioid-overdoses-in-the-shelter-system/summary-suspected-opioid-overdoses-in-shelters_test",  # noqa: E501
         "name": "summary-suspected-opioid-overdoses-in-shelters", 
         "metadata": {
             "format": "csv",
@@ -51,8 +51,8 @@ RESOURCES = {
             "fatal_overdoses_incident",
         ],
     },
-    "suspected-opiod-overdoses-in-shelters": {
-        "file_url": "http://opendata.toronto.ca/shelter.support.housing.administration/fatal-and-non-fatal-suspected-opioid-overdoses-in-the-shelter-system/suspected-opiod-overdoses-in-shelters_testQA.csv",  # noqa: E501
+    "suspected-opioid-overdoses-in-shelters": {
+        "file_url": "http://opendata.toronto.ca/shelter.support.housing.administration/fatal-and-non-fatal-suspected-opioid-overdoses-in-the-shelter-system/suspected-opioid-overdoses-in-shelters_test",  # noqa: E501
         "name": "suspected-opioid-overdoses-in-shelters", 
         "metadata": {
             "format": "csv",
@@ -274,8 +274,8 @@ with DAG(
     active_env = Variable.get("active_env")
     ckan_address = ckan_creds[active_env]["address"]
     ckan_apikey = ckan_creds[active_env]["apikey"]
-    summary_resource = RESOURCES.pop("summary-suspected-opiod-overdoses-in-shelters")
-    granular_resource = RESOURCES.pop("suspected-opiod-overdoses-in-shelters")
+    summary_resource = RESOURCES.pop("summary-suspected-opioid-overdoses-in-shelters")
+    granular_resource = RESOURCES.pop("suspected-opioid-overdoses-in-shelters")
 
     # create directories
     tmp_dir = CreateLocalDirectoryOperator(
