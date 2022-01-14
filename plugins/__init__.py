cb802aa077a86515bc58d95f29d45dc718c32e19
mackeynichols <mackey.nichols@gmail.com>
2022-01-14 18:33:56 +0000

    Revert "manual addition of packages to sys path in plugins init.py"
    This reverts commit 46df7caa3447ac0798414439e4051c55df7c5ef9.

--- plugins/__init__.py
+++
@@ -1,8 +0,0 @@
-import sys
-
-# init current dir
-dir = os.path.dirname(os.path.realpath(__file__))
-
-# add operator dependencies manually to sys path
- sys.path.insert(0, dir] + "/utils_operators" )
- sys.path.insert(0, dir] + "/ckan_operators" )
