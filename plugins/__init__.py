3f2a3be6b204415799e984ba7b2b580aa769fd87
mackeynichols <mackey.nichols@gmail.com>
2022-01-14 16:57:30 +0000

    removing init.py at plgins root folder

--- plugins/__init__.py
+++
@@ -1,9 +0,0 @@
-import sys
-import os
-
-# init current dir
-dir = os.path.dirname(os.path.realpath(__file__))
-
-# add operator dependencies manually to sys path
-sys.path.insert(0, dir + "/utils_operators" )
-sys.path.insert(0, dir + "/ckan_operators" )
