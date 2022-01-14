9b7bdb4113c572bdc2a4ce0892f365520a577b6d
mackeynichols <mackey.nichols@gmail.com>
2022-01-14 16:42:41 +0000

    removing typo from init.py

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -4,5 +4,5 @@ import sys
 dir = os.path.dirname(os.path.realpath(__file__))
 
 # add operator dependencies manually to sys path
-sys.path.insert(0, dir] + "/utils_operators" )
-sys.path.insert(0, dir] + "/ckan_operators" )
+sys.path.insert(0, dir + "/utils_operators" )
+sys.path.insert(0, dir + "/ckan_operators" )
