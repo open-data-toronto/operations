2d982d38a56ae0fe1ccba41db29efdf22661dfaa
mackeynichols <mackey.nichols@gmail.com>
2022-07-20 14:50:53 +0000

    bodysafe nested logic complete

--- plugins/ckan_operators/nested_file_readers.py
+++ plugins/ckan_operators/nested_file_readers.py
@@ -8,100 +8,100 @@ def bodysafe(data_path):
     output = []
 
     for item in input:
-
-        template = {
-                "estId": item["json"]["estId"],
-                "estName": item["json"]["estName"],
-                "addrFull": item["json"]["addrFull"],
-                "srvType": None,
-                "insStatus": None,
-                "insDate": None,
-                "observation": None,
-                "infCategory": None,
-                "defDesc": None,
-                "infType": None,
-                "actionDesc": None,
-                "OutcomeDate": None,
-                "OutcomeDesc": None,
-                "fineAmount": None,
-            }
-        print("----------- WORKING ON " + item["json"]["estName"])
-        # deal with geometry
-        template["geometry"] = json.dumps({ "type": "Point", "coordinates": [item["json"]["lon"], item["json"]["lat"] ] })
-
         for service in item["json"].get("services", None) or []:
-            template["srvType"] = service["srvType"]
-            print("------ working on service " + service["srvType"] )
 
-            # if theres no inspections, append the template to the output
+            # if theres no inspections, append the data to the output
             if not service.get("inspections", None):
-                print("===== APPENDING TEMPLATE BECAUSE INSPECTIONS IS " + str(service.get("inspections", None)))
-                template["insStatus"] = None
-                template["insDate"] = None
-                template["observation"] = None
-                template["infCategory"] = None
-                template["defDesc"] = None
-                template["infType"] = None
-                template["actionDesc"] = None
-                template["OutcomeDate"] = None
-                template["OutcomeDesc"] = None
-                template["fineAmount"] = None
-                print("xxx TEMPLATE: " + str(template))    
-                output.append( template )
+                output.append( 
+                    {
+                        "estId": item["json"]["estId"],
+                        "estName": item["json"]["estName"],
+                        "addrFull": item["json"]["addrFull"],
+                        "srvType": service["srvType"],
+                        "insStatus": None,
+                        "insDate": None,
+                        "observation": None,
+                        "infCategory": None,
+                        "defDesc": None,
+                        "infType": None,
+                        "actionDesc": None,
+                        "OutcomeDate": None,
+                        "OutcomeDesc": None,
+                        "fineAmount": None,
+                        "geometry": json.dumps({ "type": "Point", "coordinates": [item["json"]["lon"], item["json"]["lat"] ] })
+                    }
+                 )
                 
 
             for inspection in service.get("inspections", None) or []:
-                print("------- working on inspection " + inspection["insDate"])
-                template["insStatus"] = inspection["insStatus"]
-                template["insDate"] = inspection["insDate"]
-                template["observation"] = inspection["observation"]
-
-                # if theres no infractions, append the template to the output
+                # if theres no infractions, append the data to the output
                 if not inspection.get("infractions", None):
-                    print("===== APPENDING TEMPLATE BECAUSE infractions IS " + str(service.get("infractions", None)))
-                    print(inspection)
-                    print(service)
-                    print(item)
-                    template["infCategory"] = None
-                    template["defDesc"] = None
-                    template["infType"] = None
-                    template["actionDesc"] = None
-                    template["OutcomeDate"] = None
-                    template["OutcomeDesc"] = None
-                    template["fineAmount"] = None
-                    print("xxx TEMPLATE: " + str(template))
-                    output.append( template )
+                    output.append( 
+                        {
+                            "estId": item["json"]["estId"],
+                            "estName": item["json"]["estName"],
+                            "addrFull": item["json"]["addrFull"],
+                            "srvType": service["srvType"],
+                            "insStatus": inspection["insStatus"],
+                            "insDate": inspection["insDate"],
+                            "observation": inspection["observation"],
+                            "infCategory": None,
+                            "defDesc": None,
+                            "infType": None,
+                            "actionDesc": None,
+                            "OutcomeDate": None,
+                            "OutcomeDesc": None,
+                            "fineAmount": None,
+                            "geometry": json.dumps({ "type": "Point", "coordinates": [item["json"]["lon"], item["json"]["lat"] ] })
+                        }
+                    )
                     
 
                 for infraction in inspection.get("infractions", None) or []:
-                    template["infCategory"] = infraction["infCategory"]
-                    print(" ----- working on infraction " + infraction["infCategory"])
-
-                    # if theres no infractions details, append the template to the output
+                    # if theres no infractions details, append the data to the output
                     if not infraction.get("infDtl", None):
-                        print("===== APPENDING TEMPLATE BECAUSE infDtl IS " + str(service.get("infDtl", None)))
-                        template["defDesc"] = None
-                        template["infType"] = None
-                        template["actionDesc"] = None
-                        template["OutcomeDate"] = None
-                        template["OutcomeDesc"] = None
-                        template["fineAmount"] = None
-                        print("xxx TEMPLATE: " + str(template))
-                        output.append( template )
+                        output.append( 
+                            {
+                                "estId": item["json"]["estId"],
+                                "estName": item["json"]["estName"],
+                                "addrFull": item["json"]["addrFull"],
+                                "srvType": service["srvType"],
+                                "insStatus": inspection["insStatus"],
+                                "insDate": inspection["insDate"],
+                                "observation": inspection["observation"],
+                                "infCategory": infraction["infCategory"],
+                                "defDesc": None,
+                                "infType": None,
+                                "actionDesc": None,
+                                "OutcomeDate": None,
+                                "OutcomeDesc": None,
+                                "fineAmount": None,
+                                "geometry": json.dumps({ "type": "Point", "coordinates": [item["json"]["lon"], item["json"]["lat"] ] })
+                            }
+                        )
                         
 
                     for detail in infraction["infDtl"]:
-                        print(" ----- working on infdetail " + detail.get("defDesc", None) )
-                        template["defDesc"] = detail.get("defDesc", None)
-                        template["infType"] = detail.get("infType", None)
-                        template["actionDesc"] = detail.get("actionDesc", None)
-                        template["outcomeDate"] = detail.get("outcomeDate", None)
-                        template["outcomeDesc"] = detail.get("outcomeDesc", None)
-                        template["fineAmount"] = detail.get("fineAmount", None)
-
-                        print(" ========= APPENDING INFRACTION DETAILS")
-                        print("xxx TEMPLATE: " + str(template))
-                        output.append( template )
+                        # append infraction detail info, as available, to the output
+                        output.append( 
+                                {
+                                    "estId": item["json"]["estId"],
+                                    "estName": item["json"]["estName"],
+                                    "addrFull": item["json"]["addrFull"],
+                                    "srvType": service["srvType"],
+                                    "insStatus": inspection["insStatus"],
+                                    "insDate": inspection["insDate"],
+                                    "observation": inspection["observation"],
+                                    "infCategory": infraction["infCategory"],
+                                    "defDesc": detail.get("defDesc", None),
+                                    "infType": detail.get("infType", None),
+                                    "actionDesc": detail.get("actionDesc", None),
+                                    "OutcomeDate": detail.get("outcomeDate", None),
+                                    "OutcomeDesc": detail.get("outcomeDesc", None),
+                                    "fineAmount": detail.get("fineAmount", None),
+                                    "geometry": json.dumps({ "type": "Point", "coordinates": [item["json"]["lon"], item["json"]["lat"] ] })
+                                }
+                            )
     return output
         
 
