6b7c351af6c9f9225d2d9645cfca244f919f0b76
root <root@ip-10-162-48-135.ca-central-1.compute.internal>
2022-11-15 20:34:39 +0000

    NiFi Migration Part1

--- plugins/utils_operators/agol_operators.py
+++ plugins/utils_operators/agol_operators.py
@@ -27,6 +27,7 @@ AGOL_CKAN_TYPE_MAP =  {
     "esriFieldTypeDouble": "float",
     "esriFieldTypeSmallInteger": "int",
     "esriFieldTypeSingle": "text",
+    "esriFieldTypeGlobalID": "int",
 }
 
 # list of attributes that are redundant when geometry is present in a response
