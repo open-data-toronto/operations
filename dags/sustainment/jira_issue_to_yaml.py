4d093bc25867ce227246633550af65ebe1832db4
mackeynichols <mackey.nichols@gmail.com>
2022-12-07 15:54:03 +0000

    WIP Jira integration writes yamls, parses agol attributes

--- dags/sustainment/jira_issue_to_yaml.py
+++ dags/sustainment/jira_issue_to_yaml.py
@@ -6,6 +6,7 @@ import json
 import logging
 import yaml
 import os
+import re
 from datetime import datetime
 from pathlib import Path
 
@@ -24,7 +25,67 @@ from airflow.operators.python import BranchPythonOperator, PythonOperator
 
 JIRA_APIKEY = Variable.get("jira_apikey")
 JIRA_URL = "https://toronto.atlassian.net/rest/api/3/search?jql=type=11468"
-DIR_PATH = os.path.dirname(os.path.realpath(__file__))
+DIR_PATH = Path(os.path.dirname(os.path.realpath(__file__))).parent
+YAML_DIR_PATH = DIR_PATH / "datasets" / "files_to_datastore"
+
+def scrape_resource_content_from_agol(url):
+    AGOL_CKAN_TYPE_MAP = {
+        "sqlTypeFloat": "float",
+        "sqlTypeNVarchar": "text",
+        "sqlTypeInteger": "int",
+        "sqlTypeOther": "text",
+        "sqlTypeTimestamp2": "timestamp",
+        "sqlTypeDouble": "float",
+        "esriFieldTypeString": "text",
+        "esriFieldTypeDate": "timestamp",
+        "esriFieldTypeInteger": "int",
+        "esriFieldTypeOID": "int",
+        "esriFieldTypeDouble": "float",
+        "esriFieldTypeSmallInteger": "int",
+        "esriFieldTypeSingle": "text",
+        "esriFieldTypeGlobalID": "int",
+    }
+
+    DELETE_FIELDS = [
+        "longitude",
+        "latitude",
+        "shape__length",
+        "shape__area",
+        "lat",
+        "long",
+        "lon",
+        "x",
+        "y",
+        "index_",
+    ]
+
+    page = requests.get(url)
+
+    # parse html content
+    soup = BeautifulSoup(page.content, "html.parser")
+    fields = list(soup.find_all("ul")[4])
+
+    # collect attributes
+    attributes = []
+    for field in fields:
+        if field != "\n":
+            field_content = field.get_text()
+            id = field_content.split(" ")[0]
+            type = field_content.split("type: ")[1].split(",")[0]
+            # remove redudent geometry fields
+            if id.lower() not in DELETE_FIELDS:
+                attributes.append(
+                    {
+                        "id": id, 
+                        "type": AGOL_CKAN_TYPE_MAP[type], 
+                        "info": {"notes": ""}
+                    }
+                )
+
+    attributes.append({"id": "geometry", "type": "text", "info": {"notes": ""}})
+    return attributes
+
+
 
 def get_jira_issues():
     '''Returns all "Publish Dataset" Jira issues' important metadata'''
@@ -37,18 +98,24 @@ def get_jira_issues():
     # grab relevant metadata from Jira issue
     for issue in issues:
         fields = issue["fields"]
-        output.append({
+
+        # Ignore closed tickets
+        if fields["status"]["name"] == "Closed":
+            continue
+
+        # assign relevant metadata from jira to a dict
+        issue_metadata = {
             "jira_issue_id": issue["key"],
             "jira_issue_url": issue["self"],
             "title": fields["summary"],
-            #"date_published":,
-            #"dataset_category":,
+            "date_published": None,#datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f"),
+            "dataset_category": None,
             "refresh_rate": fields["customfield_12166"][-1]["value"],
             "owner_division": fields["customfield_11827"][-1]["value"],
             "owner_section": fields["customfield_11958"],
             "owner_unit": fields["customfield_11959"],
             "owner_email": fields["customfield_12243"],
-            "civic_issues": "",
+            "civic_issues": None,
             "topics": [topic["value"] 
                     for topic in fields["customfield_12246"]],
             "tags": [{"name": item, "vocabulary_id": None} 
@@ -58,7 +125,16 @@ def get_jira_issues():
             "excerpt": fields["customfield_12244"]["content"][-1]["content"][-1]["text"],
             "limitations": fields["customfield_12245"],
             "notes": fields["description"]["content"][-1]["content"][-1]["text"],
-        })
+        }
+
+        # If the issue has an AGOL link in it, extract its attributes
+        if ("https://services3.arcgis.com/b9WvedVPoizGfvfD/arcgis/rest/services/" 
+        in fields["customfield_12234"]):
+            pattern = r"https://services3.arcgis.com/b9WvedVPoizGfvfD/arcgis/rest/services/([A-Za-z0-9-_]*)/FeatureServer/0"
+            agol_url = re.search(pattern, fields["customfield_12234"])
+            attributes = scrape_resource_content_from_agol(agol_url)
+            issue_metadata = dict(**issue_metadata, **attributes)
+        output.append(issue_metadata)
 
     return output
 
@@ -66,10 +142,13 @@ def write_to_yaml(**kwargs):
     '''Receives a json input and writes it to a YAML file'''
     issues = kwargs["ti"].xcom_pull(task_ids=["get_jira_issues"])[0]
 
+    print(DIR_PATH)
+
     for issue in issues:
-        package_id = issue["title"].lower().replace(" ", "")
-        with open(DIR_PATH + "/" + package_id + ".yaml", "w") as file:
-            yaml.dump({package_id: issue}, file)
+        package_id = issue["title"].lower().replace(" ", "-")
+        filename = package_id + ".yaml"
+        with open(YAML_DIR_PATH / filename, "w") as file:
+            yaml.dump({package_id: issue}, file, sort_keys=False)
 
 
 
@@ -82,7 +161,7 @@ default_args = airflow_utils.get_default_args({
     "retries": 2,
     "retry_delay": 3,
     "on_failure_callback": task_failure_slack_alert,
-    "start_date": datetime(2022, 8, 8, 0, 0, 0),
+    "start_date": datetime(2022, 12, 8, 0, 0, 0),
     "tags": ["sustainment"],
 })
 
