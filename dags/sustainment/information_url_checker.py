3bdf224c51e8498012cecdc5094480961ee48042
od-yanan <yanan.zhang@toronto.ca>
2023-02-01 20:09:25 +0000

    PEP8 formating, handling more status code situation

--- dags/sustainment/information_url_checker.py
+++ dags/sustainment/information_url_checker.py
@@ -1,12 +1,19 @@
+"""
+    This dag checks problematic inforamtion_url on a dataset page, send notification to slack on problematic urls
+"""
+
 import requests
 import logging
-from datetime import datetime, timedelta
+from datetime import datetime
 
 from airflow import DAG
 from airflow.models import Variable
 from airflow.operators.python import PythonOperator
 from ckan_operators.package_operator import GetCkanPackageListOperator
-from utils_operators.slack_operators import GenericSlackOperator, task_failure_slack_alert
+from utils_operators.slack_operators import (
+    GenericSlackOperator,
+    task_failure_slack_alert,
+)
 
 # init hardcoded vars for these dags
 ACTIVE_ENV = Variable.get("active_env")
@@ -15,74 +22,97 @@ CKAN = CKAN_CREDS[ACTIVE_ENV]["address"]
 CKAN_APIKEY = CKAN_CREDS[ACTIVE_ENV]["apikey"]
 
 with DAG(
-    'information-url-checker',
+    "information-url-checker",
     default_args={
-                "owner": "Yanan",
-                "depends_on_past": False,
-                "email": "yanan.zhang@toronto.ca",
-                "email_on_failure": False,
-                "email_on_retry": False,
-                "retries": 1,
-                "retry_delay": 3,
-                "on_failure_callback": task_failure_slack_alert,
-                "tags": ["sustainment"]
-            },
-    description='Check if information_url(Under dataset description section) accessible, send notification to slack on failure urls',
-    schedule_interval="@once",
+        "owner": "Yanan",
+        "depends_on_past": False,
+        "email": "yanan.zhang@toronto.ca",
+        "email_on_failure": False,
+        "email_on_retry": False,
+        "retries": 1,
+        "retry_delay": 3,
+        "on_failure_callback": task_failure_slack_alert,
+        "tags": ["sustainment"],
+    },
+    description="Check if information_url(Under dataset description section) accessible, send notification to slack on failure urls",
+    schedule_interval="0 8 * * 0",
     start_date=datetime(2023, 1, 27, 0, 0, 0),
+    catchup=False,
 ) as dag:
-    
+
     def information_url_checker(**kwargs):
         ti = kwargs.pop("ti")
         package_list = ti.xcom_pull(task_ids="get_package_list")
         address = kwargs.pop("address")
-        
+
         result_info = {}
         for package_name in package_list:
             url = address + "api/3/action/package_show?id=" + package_name
-            response = requests.get(url).json()['result']
+            response = requests.get(url).json()["result"]
             if "information_url" in response.keys():
-                information_url = response['information_url']
-                if 'http' in information_url:
+                information_url = response["information_url"]
+                if "http" in information_url:
                     call = information_url
                 else:
-                    call = 'https://' + information_url
+                    call = "https://" + information_url
                 logging.info(f"{call}")
+
+                # record probametic url
                 try:
                     response = requests.get(call)
-                    status = response.status_code
+                    status_code = response.status_code
                     reason = response.reason
-                    logging.info(f"---Status-{status}---Reason--{reason}---{package_name}----{call}----------")
-                    if status == 404:
-                        result_info[package_name] = "Page Not Found:  "+ information_url
-                except:
-                    logging.info(f"---ERROR: Failed to establish connection---{package_name}----{call}")
-                    result_info[package_name] = "Hmmm… can't reach this page:  " + information_url
+                    logging.info(
+                        f"---Status-{status_code}---Reason--{reason}---{package_name}----{call}----------"
+                    )
+                    if 300 <= status_code < 400:
+                        result_info[package_name] = (
+                            "Redirected URL:  " + information_url
+                        )
+                    elif 400 <= status_code < 500:
+                        if status_code == 404:
+                            result_info[package_name] = (
+                                "Page Not Found:  " + information_url
+                            )
+                        else:
+                            result_info[package_name] = (
+                                "Client Error:  " + information_url
+                            )
+                    elif status_code >= 500:
+                        result_info[package_name] = "Server Error:  " + information_url
+
+                except requests.ConnectionError:
+                    logging.info(
+                        f"---ERROR: Failed to establish connection---{package_name}----{call}"
+                    )
+                    result_info[package_name] = (
+                        "Hmmm… can't reach this page:  " + information_url
+                    )
 
             else:
                 information_url = None
                 logging.info("--------No Information URL Found---------")
-        
+
         return {"invalid_information_url": result_info}
 
     get_package_list = GetCkanPackageListOperator(
-        task_id = "get_package_list", 
-        address=CKAN, 
+        task_id="get_package_list",
+        address=CKAN,
         apikey=CKAN_APIKEY,
     )
- 
+
     information_url_checker = PythonOperator(
         task_id="information_url_checker",
         python_callable=information_url_checker,
         op_kwargs={"address": CKAN},
     )
-    
+
     slack_notificaiton = GenericSlackOperator(
-        task_id = "slack_notificaiton",
-        message_header = "Information_URL checking",
-        message_content_task_id = "information_url_checker",
-        message_content_task_key = "invalid_information_url",
-        message_body = "Information URL!"
+        task_id="slack_notificaiton",
+        message_header="Information_URL checking",
+        message_content_task_id="information_url_checker",
+        message_content_task_key="invalid_information_url",
+        message_body="Information URL!",
     )
-    
+
     get_package_list >> information_url_checker >> slack_notificaiton
