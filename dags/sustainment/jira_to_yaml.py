fe3b2044be86beb1a3f7c04b478314da8d55954d
od-yanan <yanan.zhang@toronto.ca>
2023-02-15 21:27:10 +0000

    automatically close jira ticket

--- dags/sustainment/jira_to_yaml.py
+++ dags/sustainment/jira_to_yaml.py
@@ -1,6 +1,7 @@
 """Creates a yaml config for CoT OD Airflow ETL from an OD Jira Ticket
    Assign Jira ticket Id using Configuration JSON before triggering DAG,
-   e.g. {"jira_ticket_id": "DTSD-876"}
+   e.g. {"jira_ticket_id": "DTSD-876"}, please note string need to be
+   double quoted.
 """
 
 import requests
@@ -27,6 +28,18 @@ from airflow.operators.python import BranchPythonOperator, PythonOperator
 JIRA_URL = "https://toronto.atlassian.net/rest/api/3/search?jql=type=11468"
 DIR_PATH = Path(os.path.dirname(os.path.realpath(__file__))).parent
 YAML_DIR_PATH = DIR_PATH / "datasets" / "files_to_datastore"
+
+# headers for authenticate jira api calls
+headers = {
+    }
+
+# Create a mapping between jira issue transition name and id
+jira_issue_transitions_mapping = {
+    "In Progress": "11",
+    "Waiting on Division": "21",
+    "Closed": "31",
+    "Waiting on Open Data": "51",
+}
 
 # Create a mapping between yaml fields and jira fields
 mapping = {
@@ -67,16 +80,31 @@ with DAG(
 ) as dag:
 
     def get_jira_ticket_id(**kwargs):
-
+        # get jira ticket ids from airflow dag configuration
         if "jira_ticket_id" in kwargs["dag_run"].conf.keys():
             jira_ticket_id = kwargs["dag_run"].conf["jira_ticket_id"]
 
             assert isinstance(jira_ticket_id, list), (
-                "Input 'jira_ticket_ids' object needs to be a list of the"
+                "Input 'jira_ticket_ids' object needs to be a list of "
                 + "jira tickets you want to run"
             )
 
         return {"jira_ticket_id": jira_ticket_id}
+
+    def validate_jira_ticket(**kwargs):
+        # make sure input jira ticket id exists
+        input_ticket_ids = kwargs.pop("ti").xcom_pull(task_ids="get_jira_ticket_id")[
+            "jira_ticket_id"
+        ]
+
+        issues = json.loads(requests.get(JIRA_URL, headers=headers).content)["issues"]
+        ticket_pool = [issue["key"] for issue in issues]
+        logging.info(f"Jira Ticket Pool: {json.dumps(ticket_pool)}")
+
+        for ticket in input_ticket_ids:
+            if ticket not in ticket_pool:
+                message = f"Jira Ticket {ticket} is not valid. Please double check."
+                raise Exception(message)
 
     def grab_issue_content(issue):
 
@@ -104,7 +132,8 @@ with DAG(
                 "title": fields["summary"],
                 "date_published": datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f"),
                 "dataset_category": "Map"
-                if fields[mapping["dataset_category"]]["value"] == "Yes"
+                if fields[mapping["dataset_category"]]
+                and fields[mapping["dataset_category"]]["value"] == "Yes"
                 else None,
                 "refresh_rate": fields[mapping["refresh_rate"]]["value"]
                 if fields[mapping["refresh_rate"]]
@@ -166,23 +195,37 @@ with DAG(
             yaml.dump(issue_content, file, sort_keys=False)
         return {"filename": filename}
 
+    def close_ticket(jira_issue_id):
+        url = (
+            "https://toronto.atlassian.net/rest/api/3/issue/"
+            + jira_issue_id
+            + "/transitions"
+        )
+        payload = json.dumps(
+            {"transition": {"id": jira_issue_transitions_mapping["Closed"]}}
+        )
+
+        headers = {
+            
+        }
+
+        response = requests.request("POST", url, headers=headers, data=payload)
+        logging.info(f"Status: {response.status_code}, Ticket has been closed.")
+
     def get_all_jira_issues(**kwargs):
         """Generate all yamls for "Publish Dataset" Jira issues'"""
 
-        jira_ticket_ids = kwargs.pop("ti").xcom_pull(task_ids="get_jira_ticket_id")[
+        input_ticket_ids = kwargs.pop("ti").xcom_pull(task_ids="get_jira_ticket_id")[
             "jira_ticket_id"
         ]
-        logging.info(f"---Jira Ticket Id----{jira_ticket_ids}-----------")
-
-        headers = {
-            }
+        logging.info(f"Jira Ticket Ids: {input_ticket_ids}")
 
         # get all jira issues is and request type
         issues = json.loads(requests.get(JIRA_URL, headers=headers).content)["issues"]
-        yaml_files_list = {}
+        output_list = {}
 
         for issue in issues:
-            if issue["key"] in jira_ticket_ids:
+            if issue["key"] in input_ticket_ids:
                 request_type = issue["fields"]["customfield_10502"]["requestType"][
                     "name"
                 ]
@@ -191,17 +234,31 @@ with DAG(
                 )
 
                 issue_content = grab_issue_content(issue)
-                filename = "".join(list(issue_content.keys())) + "-test.yaml"
-                write_issue_content_to_yaml(issue_content, filename)
-                yaml_files_list[issue["key"]] = filename
+                if issue_content:
+                    filename = "".join(list(issue_content.keys())) + "-test.yaml"
+                    # write data to yaml file
+                    write_issue_content_to_yaml(issue_content, filename)
+                    # close current ticket
+                    close_ticket(issue["key"])
+                    output_list[issue["key"]] = ":done_green:" + filename
+                else:
+                    output_list[
+                        issue["key"]
+                    ] = "Request for updating existing dataset, no need to generate yaml."
             else:
                 continue
 
-        return {"generated-yaml-list": yaml_files_list}
+        return {"generated-yaml-list": output_list}
 
     get_jira_ticket_id = PythonOperator(
         task_id="get_jira_ticket_id",
         python_callable=get_jira_ticket_id,
+        provide_context=True,
+    )
+
+    validate_jira_ticket = PythonOperator(
+        task_id="validate_jira_ticket",
+        python_callable=validate_jira_ticket,
         provide_context=True,
     )
 
@@ -213,13 +270,15 @@ with DAG(
 
     slack_notificaiton = GenericSlackOperator(
         task_id="slack_notificaiton",
-        message_header=(
-            (":large_green_circle: Task Succeeded,"
-             " Succefully Generated yamls!")
-        ),
+        message_header=(" Task Succeeded, Succefully Generated :yaml: !"),
         message_content_task_id="get_all_jira_issues",
         message_content_task_key="generated-yaml-list",
         message_body="",
     )
 
-    get_jira_ticket_id >> get_all_jira_issues >> slack_notificaiton
+    (
+        get_jira_ticket_id
+        >> validate_jira_ticket
+        >> get_all_jira_issues
+        >> slack_notificaiton
+    )
