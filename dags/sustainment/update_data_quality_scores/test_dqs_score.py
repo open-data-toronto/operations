9aff4f1ad97a835bfb9968595f80d455b6290b64
od-yanan <yanan.zhang@toronto.ca>
2023-05-24 21:04:55 +0000

    WIP tests for DQS logic

---
+++ dags/sustainment/update_data_quality_scores/test_dqs_score.py
@@ -0,0 +1,94 @@
+from airflow import DAG
+from airflow.models import TaskInstance
+import os
+
+from datetime import datetime as dt
+from sustainment.update_data_quality_scores import dqs_logic, explanation_codes_logic
+import json
+import pandas as pd
+
+# init the directory where the data will be written to
+current_folder = os.path.dirname(os.path.realpath(__file__))
+
+METADATA_FIELDS = [
+    "notes",
+    "topics",
+    "owner_email",
+    "information_url",
+]
+
+
+with open(current_folder + "/death-registry-statistics-package.json") as file:
+    package = json.load(file)["result"]
+
+with open(current_folder + "/death-registry-statistics-datastore.json") as file:
+    datastore = json.load(file)["result"]
+
+df = pd.DataFrame(datastore["records"]).drop("_id", axis=1)
+content, fields = df, [x for x in datastore["fields"] if x["id"] != "_id"]
+
+
+def test_valid_parse_datetime():
+    date = "2023-05-03T16:51:13.211496"
+    assert isinstance(dqs_logic.parse_datetime(date), int)
+
+
+def test_invalid_parse_datetime():
+    date = "2022-05"
+
+    assert dqs_logic.parse_datetime(date) is None
+
+
+def test_parse_col_name():
+    word = "Test Camel_"
+    empty_word = ""
+
+    assert dqs_logic.parse_col_name(word) == ["test", "camel"]
+    assert dqs_logic.parse_col_name(empty_word) == []
+
+
+def test_score_metadata_datastore():
+    assert dqs_logic.score_metadata(package, METADATA_FIELDS, fields) == 0.875
+
+
+def test_score_metadata():
+    assert dqs_logic.score_metadata(package, METADATA_FIELDS, "") == 0.75
+
+
+def test_invalid_score_metadata():
+    assert dqs_logic.score_metadata("", METADATA_FIELDS) == 0
+
+
+def test_score_usability():
+    assert dqs_logic.score_usability(fields, content) == 1
+
+
+def test_score_completeness():
+    assert dqs_logic.score_completeness(content) == 1
+
+
+def test_attribue_description_check():
+    assert explanation_codes_logic.attribue_description_check(fields) == (False, [])
+
+
+def test_prepare_and_normalize_scores():
+    weights_datastore = [0.35, 0.35, 0.15, 0.1, 0.05]
+    weights_filestore = [0.41, 0.41, 0.18]
+    bins = BINS = {
+        "Bronze": 0.6,
+        "Silver": 0.8,
+        "Gold": 1,
+    }
+    DIMENSIONS = [
+        "metadata",
+        "freshness",
+        "accessibility",
+        "completeness",
+        "usability",
+    ]
+    
+    df = pd.read_parquet(raw_explanation_code_path).set_index(
+        ["package", "resource"], drop=True
+    )
+    
+   
