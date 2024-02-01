6c0326067183d00fad94e881e9704949ef135a30
od-yanan <yanan.zhang@toronto.ca>
2024-02-01 19:20:28 +0000

    change dqs schedule

--- dags/sustainment/update_data_quality_scores/generate_data_quality_codes.py
+++ dags/sustainment/update_data_quality_scores/generate_data_quality_codes.py
@@ -17,7 +17,7 @@ from ckan_operators.resource_operator import GetOrCreateResourceOperator
 
 job_settings = {
     "description": "Calculates DQ scores across the catalogue",
-    "schedule": "15 17 * * *",
+    "schedule": "20 19 * * *",
     "start_date": datetime(2023, 3, 27, 0, 0, 0),
 }
 
