2d9c3b8678751019bcf0534e65b80845aa8330b1
od-yanan <yanan.zhang@toronto.ca>
2024-04-02 18:32:29 +0000

    customized readers and yamls for toronto-beaches-water-quality and toronto-beaches-observations

--- plugins/readers/custom_readers.py
+++ plugins/readers/custom_readers.py
@@ -1,39 +1,24 @@
-'''Functions yielding rows (as dicts) for custom read jobs'''
+"""Functions yielding rows (as dicts) for custom read jobs"""
+
+import json
+import requests
+from airflow.models import Variable
+
 
 def test_reader():
     data = [
-        {
-            "row1": 1,
-            "row2": "text",
-            "row3": 2.11
-        },
-        {
-            "row1": 1,
-            "row2": "text",
-            "row3": 3.1
-        },
-        {
-            "row1": 2,
-            "row2": "text",
-            "row3": 3.1
-        },
-        {
-            "row1": 1,
-            "row2": "text",
-            "row3": 3.1
-        },
+        {"row1": 1, "row2": "text", "row3": 2.11},
+        {"row1": 1, "row2": "text", "row3": 3.1},
+        {"row1": 2, "row2": "text", "row3": 3.1},
+        {"row1": 1, "row2": "text", "row3": 3.1},
     ]
 
     for i in data:
         yield i
 
+
 def bodysafe():
     # custom logics to produce a flat list of dicts from the nested bodysafe input
-
-    print("------------ BODYSAFE")
-    import json
-    import requests
-    from airflow.models import Variable
 
     url = "https://secure.toronto.ca/opendata/bs_od/full_list/v1?format=json"
     user_key = Variable.get("secure_toronto_opendata_USER_KEY")
@@ -43,22 +28,47 @@ def bodysafe():
         "SRV-KEY": srv_key,
         "USER-KEY": user_key,
     }
-    
+
     raw_input = json.loads(requests.get(url, headers=headers).text)
 
     for item in raw_input:
         for service in item["json"].get("services", None) or []:
-
             # if theres no inspections, append the data to the output
             if not service.get("inspections", None):
                 yield {
+                    "estId": item["json"]["estId"],
+                    "estName": item["json"]["estName"],
+                    "addrFull": item["json"]["addrFull"],
+                    "srvType": service["srvType"],
+                    "insStatus": None,
+                    "insDate": None,
+                    "observation": None,
+                    "infCategory": None,
+                    "defDesc": None,
+                    "infType": None,
+                    "actionDesc": None,
+                    "OutcomeDate": None,
+                    "OutcomeDesc": None,
+                    "fineAmount": None,
+                    "geometry": json.dumps(
+                        {
+                            "type": "Point",
+                            "coordinates": [item["json"]["lon"], item["json"]["lat"]],
+                        }
+                    ),
+                }
+
+            for inspection in service.get("inspections", None) or []:
+                # if theres no infractions, append the data to the output
+                if not inspection.get("infractions", None):
+                    yield {
                         "estId": item["json"]["estId"],
                         "estName": item["json"]["estName"],
                         "addrFull": item["json"]["addrFull"],
                         "srvType": service["srvType"],
-                        "insStatus": None,
-                        "insDate": None,
-                        "observation": None,
+                        "insStatus": inspection["insStatus"],
+                        "insDate": inspection["insDate"],
+                        "observation": inspection["observation"],
                         "infCategory": None,
                         "defDesc": None,
                         "infType": None,
@@ -66,15 +76,21 @@ def bodysafe():
                         "OutcomeDate": None,
                         "OutcomeDesc": None,
                         "fineAmount": None,
-                        "geometry": json.dumps({ "type": "Point", "coordinates": [item["json"]["lon"], item["json"]["lat"] ] })
+                        "geometry": json.dumps(
+                            {
+                                "type": "Point",
+                                "coordinates": [
+                                    item["json"]["lon"],
+                                    item["json"]["lat"],
+                                ],
+                            }
+                        ),
                     }
-                    
-                
 
-            for inspection in service.get("inspections", None) or []:
-                # if theres no infractions, append the data to the output
-                if not inspection.get("infractions", None):
-                    yield {
+                for infraction in inspection.get("infractions", None) or []:
+                    # if theres no infractions details, append the data to the output
+                    if not infraction.get("infDtl", None):
+                        yield {
                             "estId": item["json"]["estId"],
                             "estName": item["json"]["estName"],
                             "addrFull": item["json"]["addrFull"],
@@ -82,53 +98,108 @@ def bodysafe():
                             "insStatus": inspection["insStatus"],
                             "insDate": inspection["insDate"],
                             "observation": inspection["observation"],
-                            "infCategory": None,
+                            "infCategory": infraction["infCategory"],
                             "defDesc": None,
                             "infType": None,
                             "actionDesc": None,
                             "OutcomeDate": None,
                             "OutcomeDesc": None,
                             "fineAmount": None,
-                            "geometry": json.dumps({ "type": "Point", "coordinates": [item["json"]["lon"], item["json"]["lat"] ] })
+                            "geometry": json.dumps(
+                                {
+                                    "type": "Point",
+                                    "coordinates": [
+                                        item["json"]["lon"],
+                                        item["json"]["lat"],
+                                    ],
+                                }
+                            ),
                         }
 
-                for infraction in inspection.get("infractions", None) or []:
-                    # if theres no infractions details, append the data to the output
-                    if not infraction.get("infDtl", None):
-                        yield {
-                                "estId": item["json"]["estId"],
-                                "estName": item["json"]["estName"],
-                                "addrFull": item["json"]["addrFull"],
-                                "srvType": service["srvType"],
-                                "insStatus": inspection["insStatus"],
-                                "insDate": inspection["insDate"],
-                                "observation": inspection["observation"],
-                                "infCategory": infraction["infCategory"],
-                                "defDesc": None,
-                                "infType": None,
-                                "actionDesc": None,
-                                "OutcomeDate": None,
-                                "OutcomeDesc": None,
-                                "fineAmount": None,
-                                "geometry": json.dumps({ "type": "Point", "coordinates": [item["json"]["lon"], item["json"]["lat"] ] })
-                            }
-                        
                     for detail in infraction["infDtl"]:
                         # append infraction detail info, as available, to the output
                         yield {
-                                    "estId": item["json"]["estId"],
-                                    "estName": item["json"]["estName"],
-                                    "addrFull": item["json"]["addrFull"],
-                                    "srvType": service["srvType"],
-                                    "insStatus": inspection["insStatus"],
-                                    "insDate": inspection["insDate"],
-                                    "observation": inspection["observation"],
-                                    "infCategory": infraction["infCategory"],
-                                    "defDesc": detail.get("defDesc", None),
-                                    "infType": detail.get("infType", None),
-                                    "actionDesc": detail.get("actionDesc", None),
-                                    "OutcomeDate": detail.get("outcomeDate", None),
-                                    "OutcomeDesc": detail.get("outcomeDesc", None),
-                                    "fineAmount": detail.get("fineAmount", None),
-                                    "geometry": json.dumps({ "type": "Point", "coordinates": [item["json"]["lon"], item["json"]["lat"] ] })
+                            "estId": item["json"]["estId"],
+                            "estName": item["json"]["estName"],
+                            "addrFull": item["json"]["addrFull"],
+                            "srvType": service["srvType"],
+                            "insStatus": inspection["insStatus"],
+                            "insDate": inspection["insDate"],
+                            "observation": inspection["observation"],
+                            "infCategory": infraction["infCategory"],
+                            "defDesc": detail.get("defDesc", None),
+                            "infType": detail.get("infType", None),
+                            "actionDesc": detail.get("actionDesc", None),
+                            "OutcomeDate": detail.get("outcomeDate", None),
+                            "OutcomeDesc": detail.get("outcomeDesc", None),
+                            "fineAmount": detail.get("fineAmount", None),
+                            "geometry": json.dumps(
+                                {
+                                    "type": "Point",
+                                    "coordinates": [
+                                        item["json"]["lon"],
+                                        item["json"]["lat"],
+                                    ],
                                 }
+                            ),
+                        }
+
+
+def toronto_beaches_water_quality():
+    url = "https://secure.toronto.ca/opendata/adv_od/beach_results/v1?format=json&startDate=2000-01-01&endDate=9999-01-01"
+    user_key = Variable.get("secure_toronto_opendata_USER_KEY")
+    srv_key = Variable.get(
+        "toronto-beaches-water-quality_secure_toronto_opendata_SRV_KEY"
+    )
+
+    headers = {
+        "SRV-KEY": srv_key,
+        "USER-KEY": user_key,
+    }
+
+    raw_input = json.loads(requests.get(url, headers=headers).text)
+
+    for item in raw_input:
+        yield {
+            "beachId": item["beachId"],
+            "beachName": item["beachName"],
+            "siteName": item["siteName"],
+            "collectionDate": item["collectionDate"],
+            "eColi": item["eColi"],
+            "comments": item["comments"],
+            "geometry": json.dumps(
+                {"type": "Point", "coordinates": [item["lon"], item["lat"]]}
+            ),
+        }
+
+
+def toronto_beaches_observations():
+    url = "https://secure.toronto.ca/opendata/adv_od/route_observations/v1?format=json"
+    user_key = Variable.get("secure_toronto_opendata_USER_KEY")
+    srv_key = Variable.get(
+        "toronto-beaches-water-quality_secure_toronto_opendata_SRV_KEY"
+    )
+
+    headers = {
+        "SRV-KEY": srv_key,
+        "USER-KEY": user_key,
+    }
+
+    raw_input = json.loads(requests.get(url, headers=headers).text)
+
+    for item in raw_input:
+        print(item)
+        yield {
+            "dataCollectionDate": item["dataCollectionDate"],
+            "beachName": item["beachName"],
+            "windSpeed": item["wind_speed"],
+            "windDirection": item["windDirection"],
+            "airTemp": item["airTemp"],
+            "rain": item["rain"],
+            "rainAmount": item["rainAmount"],
+            "waterTemp": item["waterTemp"],
+            "waterFowl": item["waterFowl"],
+            "waveAction": item["waveAction"],
+            "waterClarity": item["waterClarity"],
+            "turbidity": item["turbidity"],
+        }
