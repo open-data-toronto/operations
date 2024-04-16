519795c6ed9aa0cf2cb26a6ea6ca1b58ea1f25d1
root <root@ip-10-162-44-128.ca-central-1.compute.internal>
2024-04-16 16:04:09 +0000

    making sure git knows operations repo is safe before running 'git pull'

--- dags/sustainment/pull_latest_code.py
+++ dags/sustainment/pull_latest_code.py
@@ -9,7 +9,7 @@ with DAG(
     default_args=airflow_utils.get_default_args(
         {"retries": 0, "start_date": datetime(2020, 11, 10, 0, 30, 0)}
     ),
-    description="Pulls repo code. Updated dags must be deleted and restarted.",
+    description="Pulls repo code.",
     schedule_interval="*/5 * * * *",
     tags=["sustainment"],
     catchup=False,
@@ -17,7 +17,7 @@ with DAG(
 
     pull_repo = BashOperator(
         task_id="pull_repo",
-        bash_command=f"git -C {Variable.get('repo_dir')} pull; echo $?",
+        bash_command=f"git config --global --add safe.directory /data/operations; git -C {Variable.get('repo_dir')} pull; echo $?",
     )
 
     pull_repo
